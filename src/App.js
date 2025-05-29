46517bc0f8bd1078207d9a32906b83fb8d97d375
SilasExpert <syllas-@hotmail.com>
2025-05-29 18:36:52 -0300

    criação de componentes e navegação entre páginas

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 
+//import de components
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
+
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
