13c45e5ab62d2fc9d70e3099dfdce5b7cc05109b
SilasExpert <syllas-@hotmail.com>
2025-05-30 02:19:53 -0300

    criação de componentes e navegação entre páginas

--- src/App.js
+++ src/App.js
@@ -7,25 +7,34 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
+import Info from './pages/Info';
+import NotFound from './pages/NotFound';
+import Search from './pages/Search';
 
 //import de components
 import Navbar from './components/Navbar';
-import Info from './pages/Info';
+import SearchForm from './components/SearchForm';
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
-
       <BrowserRouter>
+        {/* links com react router */}
         <Navbar />
+        {/* search */}
+        <SearchForm />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
-          {/* Rota dinâmica */}
-          <Route path='/products/:id' element={<Product />} />
           {/* Nested routes */}
           <Route path='/products/:id/info' element={<Info />} />
+          {/* Rota dinâmica */}
+          <Route path='/products/:id' element={<Product />} />
+          {/* Search params*/}
+          <Route path='/search' element={<Search />} />
+          {/* no match route, NotFound - 404 */}
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
