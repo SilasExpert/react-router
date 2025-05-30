7b1cfd186adbc7529cda1959ce215702f70ed4f9
SilasExpert <syllas-@hotmail.com>
2025-05-29 22:41:51 -0300

    implementação de carregamento dinâmico dos dados

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,12 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/UseFetch";
 
 const Product = () => {
     //Rota dinâmica
     const { id } = useParams();
 
+    //carregamento de dado individual
+    const url = "http://localhost:3000/products/" + id;
+
+    const {data: product, loading, error} = useFetch(url);
+
     return (
         <>
             <p>ID do produto: {id}</p>
+            {error && <p>Ocorreu um erro!</p>}
+            {loading && <p>Carregando dados...</p>}
+            {product && (
+                <div>
+                    <h1>{product.nome}</h1>
+                    <p>R$ {product.preco}</p>
+                </div>
+            )}
         </>
     );
 };
